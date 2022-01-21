115485c1a08eb022358daa4f6fec857c52979294
James Odell <jpodell21@gmail.com>
2022-01-21 12:36:53 -0700

    Lesson 3.14 ES6 Arrow Function part 1

--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -1,16 +1,21 @@
-"use strict";
-
 // function square(x) {
 //     return x * x;
 // };
+
 // console.log(square(3));
+
 // // const squareArrow = (x) => {
 // //     return x * x;
 // // };
+
 // const squareArrow = (x) => x * x;
+
 // console.log(squareArrow(4));
-var getFirstName = function getFirstName(fullName) {
-  return fullName.split(' ')[0];
-};
+
+// const getFirstName = (fullName) => {
+//     return fullName.split(' ')[0];
+// };
+
+const getFirstName = (fullName) => fullName.split(' ')[0];
 
 console.log(getFirstName('Billy Bob'));
