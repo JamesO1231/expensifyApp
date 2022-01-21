20abe9d23a709910ade70683d51e7b4a35d6a59e
James Odell <jpodell21@gmail.com>
2022-01-21 13:20:53 -0700

    Lesson 3.15 ES6 arrow function part II

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,42 @@
 "use strict";
 
-// function square(x) {
-//     return x * x;
-// };
-// console.log(square(3));
-// // const squareArrow = (x) => {
-// //     return x * x;
-// // };
-// const squareArrow = (x) => x * x;
-// console.log(squareArrow(4));
-var getFirstName = function getFirstName(fullName) {
-  return fullName.split(' ')[0];
+var add = function add(a, b) {
+  // console.log(arguments);
+  return a + b;
 };
 
-console.log(getFirstName('Billy Bob'));
+console.log(add(55, 1)); // const user = {
+//     name: 'Billy Bob',
+//     cities: ['Denver', 'New York', 'Austin'],
+//     printPlacesLived() {
+//         this.cities.forEach((city) => {
+//             console.log(this.name + ' has lived in ' + city);
+//         });
+//     }
+// };
+// user.printPlacesLived();
+
+var user = {
+  name: 'Billy Bob',
+  cities: ['Denver', 'New York', 'Austin'],
+  printPlacesLived: function printPlacesLived() {
+    var _this = this;
+
+    return this.cities.map(function (city) {
+      return _this.name + ' has lived in ' + city;
+    });
+  }
+};
+console.log(user.printPlacesLived());
+var multiplier = {
+  numbers: [10, 20, 30],
+  multiplyBy: 3,
+  multiply: function multiply() {
+    var _this2 = this;
+
+    return this.numbers.map(function (number) {
+      return number * _this2.multiplyBy;
+    });
+  }
+};
+console.log(multiplier.multiply());
