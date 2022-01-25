aeda0add2d25692035ca629efcb396524f6bc233
James Odell <jpodell21@gmail.com>
2022-01-24 20:30:33 -0700

    Lesson 3.16 Events and Attributes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,42 +1,53 @@
 "use strict";
 
-var add = function add(a, b) {
-  // console.log(arguments);
-  return a + b;
+console.log('App.js is running');
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Give in to the power of the computer',
+  options: ['One', 'Two']
 };
-
-console.log(add(55, 1)); // const user = {
-//     name: 'Billy Bob',
-//     cities: ['Denver', 'New York', 'Austin'],
-//     printPlacesLived() {
-//         this.cities.forEach((city) => {
-//             console.log(this.name + ' has lived in ' + city);
-//         });
-//     }
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("p", null, app.subtitle), /*#__PURE__*/React.createElement("p", null, app.options.length > 0 ? 'Here are your options' : 'No options'), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "First"), /*#__PURE__*/React.createElement("li", null, "Second"))); // const user = {
+//   name: 'Bob',
+//   age: 30,
+//   location: 'Denver',
 // };
-// user.printPlacesLived();
+// function getLocation(location) {
+//   if (location) {
+//     return <p>Location: {location}</p>;
+//   } 
+// };
+// const userName = 'Mike'
+// const userAge = 27;
+// const userLocation = 'Denver';
+// const templateTwo = (
+//   <div>
+//     <h1>{user.name ? user.name : 'Anonymous'}</h1>
+//     {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+//     {getLocation(user.location)}
+//   </div>
+// );
 
-var user = {
-  name: 'Billy Bob',
-  cities: ['Denver', 'New York', 'Austin'],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
+var count = 0;
 
-    return this.cities.map(function (city) {
-      return _this.name + ' has lived in ' + city;
-    });
-  }
+var addOne = function addOne() {
+  console.log('addOne');
 };
-console.log(user.printPlacesLived());
-var multiplier = {
-  numbers: [10, 20, 30],
-  multiplyBy: 3,
-  multiply: function multiply() {
-    var _this2 = this;
 
-    return this.numbers.map(function (number) {
-      return number * _this2.multiplyBy;
-    });
-  }
+var minusOne = function minusOne() {
+  console.log('minusOne');
 };
-console.log(multiplier.multiply());
+
+var reset = function reset() {
+  console.log('reset');
+};
+
+var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Count: ", count), /*#__PURE__*/React.createElement("button", {
+  onClick: addOne
+}, "+1"), /*#__PURE__*/React.createElement("button", {
+  onClick: minusOne
+}, "-1"), /*#__PURE__*/React.createElement("button", {
+  onClick: reset
+}, "Reset"));
+console.log(templateTwo);
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot);
