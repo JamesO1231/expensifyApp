3cf005fd64d84e31dacc4e9532b0b810222e4273
James Odell <jpodell21@gmail.com>
2022-01-26 13:09:37 -0700

    Lesson 3.21 Build It, visibility toggle

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,72 +1,17 @@
 "use strict";
 
-console.log('App.js is running');
-var app = {
-  title: 'Indecision App',
-  subtitle: 'Give in to the power of the computer',
-  options: []
-};
+var changeVisibility = false;
 
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
-  var option = e.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    render();
-  }
-};
-
-var removeAll = function removeAll() {
-  app.options = [];
+var toggleVisibility = function toggleVisibility() {
+  changeVisibility = !changeVisibility;
   render();
 };
 
-var onMakeDecision = function onMakeDecision() {
-  var randomNum = Math.floor(Math.random() * app.options.length);
-  var option = app.options[randomNum];
-  alert(option);
-};
-
-var appRoot = document.getElementById('app');
-
 var render = function render() {
-  var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("p", null, app.subtitle), /*#__PURE__*/React.createElement("p", null, app.options.length > 0 ? 'Here are your options' : 'No options'), /*#__PURE__*/React.createElement("button", {
-    disabled: app.options.length === 0,
-    onClick: onMakeDecision
-  }, "What should I do?"), /*#__PURE__*/React.createElement("button", {
-    onClick: removeAll
-  }, "Remove All"), /*#__PURE__*/React.createElement("ol", null, app.options.map(function (option) {
-    return /*#__PURE__*/React.createElement("li", {
-      key: option
-    }, option);
-  })), /*#__PURE__*/React.createElement("form", {
-    onSubmit: onFormSubmit
-  }, /*#__PURE__*/React.createElement("input", {
-    type: "text",
-    name: "option"
-  }), /*#__PURE__*/React.createElement("button", null, "Add Option")));
-  ReactDOM.render(template, appRoot);
+  var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Visibility Toggle"), /*#__PURE__*/React.createElement("button", {
+    onClick: toggleVisibility
+  }, changeVisibility ? 'Hide details' : 'Show details'), changeVisibility && /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, "Look at all of these secrets I've been hiding!")));
+  ReactDOM.render(template, document.getElementById('app'));
 };
 
-render(); // const user = {
-//   name: 'Bob',
-//   age: 30,
-//   location: 'Denver',
-// };
-// function getLocation(location) {
-//   if (location) {
-//     return <p>Location: {location}</p>;
-//   } 
-// };
-// const userName = 'Mike'
-// const userAge = 27;
-// const userLocation = 'Denver';
-// const templateTwo = (
-//   <div>
-//     <h1>{user.name ? user.name : 'Anonymous'}</h1>
-//     {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-//     {getLocation(user.location)}
-//   </div>
-// );
+render();
