7bc9e72f848a4e3b53c6c663c8daebfeea763ce2
James Odell <jpodell21@gmail.com>
2022-01-27 20:52:45 -0700

    Lesson S4E27 Nesting Components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -24,6 +37,17 @@ class Options extends React.Component {
     return (
       <div>
         <h2>What would you like for dinner?</h2>
+        <Option />
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>This is your option</h3>
       </div>
     );
   }
@@ -35,17 +59,8 @@ class AddOption extends React.Component {
       <div>
         <h2>Add an option</h2>
       </div>
-    )
+    );
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
