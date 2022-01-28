d43d0efd094756f893d0a1dbb0d92b48f37db267
James Odell <jpodell21@gmail.com>
2022-01-27 20:43:09 -0700

    Lesson S4E26 React Compnents

--- src/app.js
+++ src/app.js
@@ -1,76 +1,51 @@
-console.log('App.js is running');
-
-const app = {
-  title: 'Indecision App',
-  subtitle: 'Give in to the power of the computer',
-  options: []
-};
-
-const onFormSubmit = (e) => {
-  e.preventDefault();
-  const option = e.target.elements.option.value;
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    render();
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision</h1>
+        <h2>Put your life in the hands of the computer</h2>
+      </div>
+    );
   }
-};
+}
 
-const removeAll = () => {
-  app.options = [];
-  render();
-};
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
 
-const onMakeDecision = () => {
-  const randomNum = Math.floor(Math.random() * app.options.length);
-  const option = app.options[randomNum];
-  alert(option);
-};
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>What would you like for dinner?</h2>
+      </div>
+    );
+  }
+}
 
-const appRoot = document.getElementById('app');
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Add an option</h2>
+      </div>
+    )
+  }
+}
 
-const render = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
-      <button onClick={removeAll}>Remove All</button>
-      <ol>
-        {
-          app.options.map((option) => <li key={option}>{option}</li>)
-        }
-      </ol>
-      <form onSubmit={onFormSubmit}>
-        <input type='text' name='option'/>
-        <button>Add Option</button>
-      </form>
-    </div>
-  );
-  ReactDOM.render(template, appRoot);
-};
+const jsx = (
+  <div>
+    <Header />
+    <Action />
+    <Options />
+    <AddOption />
+  </div>
+);
 
-render(); 
-
-
-// const user = {
-//   name: 'Bob',
-//   age: 30,
-//   location: 'Denver',
-// };
-// function getLocation(location) {
-//   if (location) {
-//     return <p>Location: {location}</p>;
-//   } 
-// };
-// const userName = 'Mike'
-// const userAge = 27;
-// const userLocation = 'Denver';
-// const templateTwo = (
-//   <div>
-//     <h1>{user.name ? user.name : 'Anonymous'}</h1>
-//     {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-//     {getLocation(user.location)}
-//   </div>
-// );
+ReactDOM.render(jsx, document.getElementById('app'));
