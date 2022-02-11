5e87dafb142a84f16f3771af145264474a8b59ab
James Odell <jpodell21@gmail.com>
2022-02-11 13:59:34 -0700

    installing and importing npm packages

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 // import './utils';
-import subtract, { square, add } from './utils';
+// import subtract, { square, add } from './utils';
 
-console.log('app.js is running');
-console.log(square(4));
-console.log(add(100, 23));
-console.log(subtract(100, 81));
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(100, 23));
+// console.log(subtract(100, 81));
 
-import isSenior, { isAdult, canDrink } from './person';
-console.log(isAdult(18));
-console.log(canDrink(20));
-console.log(isSenior(65));
+// import isSenior, { isAdult, canDrink } from './person';
+// console.log(isAdult(18));
+// console.log(canDrink(20));
+// console.log(isSenior(65));
 
+// import validator from 'validator';
+
+// console.log(validator.isEmail('test@gmail.com'));
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = React.createElement('p', {}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
