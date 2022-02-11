0f8e844c0e1c3b69254f210d884f8603531c8bc8
James Odell <jpodell21@gmail.com>
2022-02-11 13:32:15 -0700

    ES6 import/export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+// import './utils';
+import { square, add } from './utils';
+
 console.log('app.js is running');
+console.log(square(4));
+console.log(add(100, 23));
+
+import { isAdult, canDrink } from './person';
+console.log(isAdult(18));
+console.log(canDrink(20));
