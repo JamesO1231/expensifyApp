d26e838b74c188ec7a0a8c334403c6ddb325ee3d
James Odell <jpodell21@gmail.com>
2022-02-14 13:58:28 -0700

    ES6 class properties

--- src/app.js
+++ src/app.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/indecisionApp';
 
-  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
