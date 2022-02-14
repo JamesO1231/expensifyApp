cb8a9d1b92fd77825b41e2477879584ccdee483e
James Odell <jpodell21@gmail.com>
2022-02-14 14:12:26 -0700

    deleting children component

--- src/app.js
+++ src/app.js
@@ -2,14 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/indecisionApp';
 
-const Layout = (props) => {
-  return (
-    <div>
-     <p>Header</p>
-     {props.children}
-     <p>Footer</p>
-    </div>
-  );
-}
-
-ReactDOM.render(<Layout><p>This is inline</p></Layout>, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
