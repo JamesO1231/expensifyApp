c3b12e8a0a78b0c88b7a6ddd4a0e321865825ec6
James Odell <jpodell21@gmail.com>
2022-02-16 12:16:29 -0700

    refactoring Action, header, option, options

--- src/components/options.js
+++ src/components/options.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import Option from './option'
 
-const Options = (props) => {
-    return (
-      <div>
-      {props.options.length === 0 && <p>Please add an option to start</p>}
-        {
-          props.options.map((option) => (
-            <Option 
-              key={option} 
-              optionText={option}
-              removeOption={props.removeOption}
-            />
-          ))
-        }
-        <button onClick={props.deleteOptions}>Remove All</button>
-      </div>
-    );
-};
+const Options = (props) => (
+  <div>
+  {props.options.length === 0 && <p>Please add an option to start</p>}
+    {
+      props.options.map((option) => (
+        <Option 
+          key={option} 
+          optionText={option}
+          removeOption={props.removeOption}
+        />
+      ))
+    }
+    <button onClick={props.deleteOptions}>Remove All</button>
+  </div>
+);
 
 export default Options;
