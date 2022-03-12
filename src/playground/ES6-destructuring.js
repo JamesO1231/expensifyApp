64d8c81c3ea885813d6f3e354bcab08493e17f2b
James Odell <jpodell21@gmail.com>
2022-03-12 11:44:02 -0700

    Array destructuring

--- src/playground/ES6-destructuring.js
+++ src/playground/ES6-destructuring.js
@@ -1,3 +1,5 @@
+//Object destructuring
+
 // const person = {
 //     name: 'Andrew',
 //     age: 34,
@@ -19,14 +21,29 @@
 //     console.log(`It is ${temperature} in ${city}`);
 // }
 
-const book = {
-    title: 'Sedimentary Geology',
-    author: 'Donald R. Prothero & Fred Schwab',
-    publisher: {
-        // name: 'W. H. Freeman and Company'
-    }
-};
+// const book = {
+//     title: 'Sedimentary Geology',
+//     author: 'Donald R. Prothero & Fred Schwab',
+//     publisher: {
+//         // name: 'W. H. Freeman and Company'
+//     }
+// };
 
-const { name: publisherName = 'Self-Published' } = book.publisher;
+// const { name: publisherName = 'Self-Published' } = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+
+//Array destructuring
+
+const address = ['1234 Way Lane', 'Denver', , '654321'];
+
+const [street, city, state = 'New York', zip] = address;
+
+console.log(`You are in ${city} ${state} at ${street}`);
+
+const item = ['black coffee', '$2.00', '$2.50', '$3.00']
+
+const [coffee, small, medium, large] = item;
+
+console.log(`You ordered a small ${coffee} that cost ${small}`);
