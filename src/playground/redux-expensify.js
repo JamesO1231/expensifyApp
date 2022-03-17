b21f531b3b5ebd0f1bde3155e2244c15a4e61636
James Odell <jpodell21@gmail.com>
2022-03-17 16:39:33 -0600

    spread objects

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -24,6 +24,17 @@ const removeExpense = ({ id } = {}) => ({
     id
 });
 
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
@@ -35,6 +46,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
             ];
         case 'REMOVE_EXPENSE':
             return state.filter(({ id }) => id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    };
+                } else {
+                    return expense
+                };
+            });
         default: 
           return state;
     }
@@ -49,6 +71,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };
         default: 
           return state;
     }
@@ -69,6 +96,8 @@ const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 
 const expenseTwo = store.dispatch(addExpense({ description: 'Gas', amount: 100000000 }));
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500000000000 }));
+store.dispatch(setTextFilter('rent'));
 
 const demoState = {
     expenses: [{
@@ -86,3 +115,13 @@ const demoState = {
     }
 };
 
+const user = {
+    name: 'Bill',
+    age: 55
+};
+
+console.log({
+    ...user,
+    location: 'Denver',
+    age: 49
+});
