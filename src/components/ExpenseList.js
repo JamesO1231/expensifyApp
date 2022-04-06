e8add39a14bd2fa00afca39e736a465fcdf4f977
James Odell <jpodell21@gmail.com>
2022-04-06 12:21:59 -0600

    edit expense function and cleaning up code

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,18 +5,18 @@ import selectExpense from '../selectors/expenses';
 
 
 const ExpenseList = (props) => (
-    <div>
-      <h1>Expense List</h1>
-      {props.expenses.map((expense) => {
-        return <ExpenseListItem key={expense.id} {...expense} />
-      })};
-    </div>
+  <div>
+    <h1>Expense List</h1>
+    {props.expenses.map((expense) => {
+      return <ExpenseListItem key={expense.id} {...expense} />
+    })};
+  </div>
 );
 
 const mapStateToProps = (state) => {
-    return {
-        expenses: selectExpense(state.expenses, state.filters)
-    };
+  return {
+      expenses: selectExpense(state.expenses, state.filters)
+  };
 };
 
 export default connect(mapStateToProps)(ExpenseList);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
+import { Link } from 'react-router-dom';
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
-    <div>
+  <div>
+    <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
-      <p>{amount} - {createdAt}</p>
-      <button onClick={() => {
-        dispatch(removeExpense({ id }));
-      }}>Remove</button>
-    </div>
+    </Link>
+    <p>{amount} - {createdAt}</p>
+  </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
